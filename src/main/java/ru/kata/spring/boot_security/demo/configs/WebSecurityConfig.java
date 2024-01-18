24eb5151561120a7de6dab708f07e3152cc1535c
Alexey030383 <alexey030383@gmail.com>
2024-01-18 15:49:22 +0300

    PP_3.1.5_update

--- src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -38,7 +38,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
                 .successHandler(successUserHandler)
                 .permitAll()
                 .and()
